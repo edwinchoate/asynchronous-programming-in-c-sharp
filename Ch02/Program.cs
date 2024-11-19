530fb613279f2a97ed3bf1580dbf2a2cddbc5463
Edwin Choate <edwin.choate@pm.me>
2024-11-19 17:24:03 -0500

    adds Ch02 code proj

---
+++ Ch02/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// This represents a blocking synchronous function
+using System.Runtime.CompilerServices;
+
+void ProcessData () 
+{
+    Console.WriteLine("Processing data...");
+    Thread.Sleep(3000);
+    Console.WriteLine("Processing complete.");
+}
+
+
+Console.WriteLine("Program starting...");
+
+// ProcessData(); // BLOCKING
+Task.Run(ProcessData); // NON-BLOCKING
+
+HttpClient client = new();
+Task<HttpResponseMessage> responseTask = client.GetAsync("http://nfl.com");
+
+// HttpResponseMessage response = responseTask.Result; // BLOCKING
+
+// NON-BLOCKING
+responseTask.ContinueWith(task => {
+    HttpResponseMessage response = task.Result;
+});
+
+Console.WriteLine("Ready for input.");
+Console.ReadLine();
