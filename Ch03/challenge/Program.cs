f6f4590f8c1d1e6d9b2897d44e810c2c284f6f3d
Edwin Choate <edwin.choate@pm.me>
2024-11-20 17:43:39 -0500

    completes ch. 3 code challenge

--- Ch03/challenge/Program.cs
+++ Ch03/challenge/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+async void PrintUrl (string url) 
+{
+    try 
+    {
+        Thread.Sleep(5000);
+        HttpClient client = new();
+        string contents = await client.GetStringAsync(url);
+        Console.WriteLine(contents);
+    }
+    catch (Exception e) 
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+
+Console.WriteLine("Program start.");
+
+Console.WriteLine("Starting background process...");
+
+PrintUrl("https://example.com");
+
+Console.WriteLine("Continuing program execution.");
+Console.WriteLine();
+
+Console.ReadLine();
+Console.WriteLine("Program end.");
