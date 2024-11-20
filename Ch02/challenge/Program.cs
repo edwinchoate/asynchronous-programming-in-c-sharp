1e3382e69cb08e3c1f5275c464852f334cf1d8c9
Edwin Choate <edwin.choate@pm.me>
2024-11-20 11:07:36 -0500

    completes ch. 2 code challenge

---
+++ Ch02/challenge/Program.cs
@@ -0,0 +1,35 @@
+﻿
+List<int> CalculateFactors (int n) 
+{
+    List<int> factors  = new();
+
+    for (int i = 1; i <= n; i++) 
+    {
+        if (n % i == 0) 
+            factors.Add(i);
+    }
+    
+    return factors;
+}
+
+Console.WriteLine("Program start.");
+
+Console.WriteLine("Starting background thread...");
+
+Task<List<int>> calcFactorsTask = Task.Run(() => CalculateFactors(100));
+
+Console.WriteLine("Background thread started.");
+
+calcFactorsTask.ContinueWith(task => {
+    foreach (int i in task.Result) 
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine("Background thread complete.");
+});
+
+Console.WriteLine("Continuing program...");
+Console.ReadLine();
+
+Console.WriteLine("Program end.");
