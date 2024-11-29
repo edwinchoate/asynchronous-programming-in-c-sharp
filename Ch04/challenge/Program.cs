3cac7d5cbd2e11796baea4ec12136ca092489ab3
Edwin Choate <edwin.choate@pm.me>
2024-11-29 13:43:06 -0500

    completes course

--- Ch04/challenge/Program.cs
+++ Ch04/challenge/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+async Task GetPage (string Uri) 
+{
+    HttpClient client = new();
+    CancellationTokenSource tokenSource = new();
+    tokenSource.CancelAfter(1);
+
+    try 
+    {
+        string body = await client.GetStringAsync(Uri, tokenSource.Token);
+        Console.WriteLine(body);
+    }
+    catch (TaskCanceledException) 
+    {
+        Console.WriteLine("Error: Request timed out.");
+    }
+    finally 
+    {
+        tokenSource.Dispose();
+    }
+}
+
+
+Console.WriteLine("Starting program.");
+
+Console.WriteLine("Calling get page...");
+
+Task task = GetPage("http://example.com");
+
+Console.WriteLine("Called get page.");
+
+Console.WriteLine("Program end. Press any key to exit.");
+Console.ReadKey();
